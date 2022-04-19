18c766b16fba3918f96a5665e5f77a94b41eb817
zeno3618 <zeno3618@gmail.com>
2022-04-19 18:27:50 +0900

    update .env.example & hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,23 @@ import "solidity-coverage";
 
 dotenv.config();
 
+const chainIds = {
+  bscMainnet: 56,
+  bscTestnet: 97,
+  ganache: 1337,
+  hardhat: 31337,
+  ethKovan: 42,
+  ethMainnet: 1,
+  ethRopsten: 3,
+};
+
+const {
+  MORALIS_SPEEDY_NODE_KEY, 
+  PRIVATE_KEY, 
+  BSCSCAN_API_KEY, 
+  REPORT_GAS
+} = process.env;
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -23,20 +40,42 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.2",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      chainId: chainIds.hardhat,
+      forking: { // mainnet fork
+        enabled: true,
+        url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_SPEEDY_NODE_KEY}/bsc/mainnet`
+      }
     },
+    testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: chainIds.bscTestnet,
+      gasPrice: 20000000000,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : []
+    },
+    mainnet: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: chainIds.bscMainnet,
+      gasPrice: 20000000000,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : []
+    }
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
+    enabled: REPORT_GAS !== undefined,
     currency: "USD",
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: BSCSCAN_API_KEY,
   },
 };
 
